70cb5300d88115f0e5af459e766eed7586d84c4d
Audun Hammer Hovda <root@DESKTOP-B8218I0>
2023-05-11 21:24:58 +0200

    feat: multiple

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,13 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 struct Args {
     #[arg(short, long)]
-    innlegg: Option<String>,
+    innlegg: Option<Vec<String>>,
 
     #[arg(short, long)]
-    replikk: Option<String>,
-
-    #[arg(short, long)]
-    stryk: Option<String>,
+    replikk: Option<Vec<String>>,
 
     #[arg(short, long)]
     file: Option<PathBuf>,
-
-    #[arg(short, long)]
-    neste: bool,
 
     #[arg(short, long)]
     clear: bool,
@@ -26,32 +20,60 @@ struct Args {
 fn main() {
     let args = Args::parse();
 
-    // file
-    let path = match args.file {
-        Some(path) => path.into_os_string().into_string().unwrap(),
-        None => "talerliste.txt".to_string(),
+    let path: PathBuf = match args.file {
+        Some(p) => p,
+        None => ["talerliste.txt"].iter().collect(),
     };
 
     if args.clear {
-        clear(path.to_owned());
-        write(path.to_owned(), "---Talerliste---".to_string());
+        clear(&path);
     }
 
     match args.innlegg {
-        Some(i) => write(path.to_owned(), i),
+        Some(names) => {
+            for name in names.iter() {
+                write(&path, name.to_string());
+            }
+        }
         None => (),
     }
+
+    match args.replikk {
+        Some(names) => {
+            for name in names.iter() {
+                write(&path, "\t".to_owned() + name);
+            }
+        }
+        None => (),
+    }
+
+    print(read(&path).as_str());
 }
 
-fn read(path: String) -> String {
-    let data = fs::read_to_string(path).expect("Unable to read file");
+fn print(s: &str) {
+    s.lines()
+        .map(|line| {
+            println!("{}", line.trim_end());
+        })
+        .collect()
+}
+
+fn read(path: &PathBuf) -> String {
+    let data = match fs::read_to_string(path) {
+        Ok(f) => f,
+        Err(_) => {
+            clear(path);
+            return fs::read_to_string(path).unwrap();
+        }
+    };
     data
 }
 
-fn clear(path: String) {
-    fs::write(path, "").expect("Unable to write file");
+fn clear(path: &PathBuf) {
+    fs::write(path, "---Talerlise---\n").expect("Unable to write file");
 }
-fn write(path: String, line: String) {
-    let file = read(path.to_owned());
+
+fn write(path: &PathBuf, line: String) {
+    let file: String = read(path);
     fs::write(path, file + &line + "\n").expect("Unable to write file");
 }
