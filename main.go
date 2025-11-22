93a70b1207f52ff05fe7986fc30ea2c2334478d0
Audun Hammer Hovda <hammerhovda@gmail.com>
2025-11-22 15:32:15 +0000

    added air

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "log/slog"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	slog.Info("Hello world")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello world"))
+	})
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatalln(err)
+
+	}
 }
